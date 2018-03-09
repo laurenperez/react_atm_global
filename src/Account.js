819a17bcacbae3ec23ebd3358061858eb9f8e752
laurenperez <laurenashleymarks@gmail.com>
2018-03-09 12:23:10 -0800

    transfer functionality added

--- src/Account.js
+++ src/Account.js
@@ -3,41 +3,37 @@ import React, { Component } from 'react';
 export default class Account extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      balance: 0
-    }
-
-    this.handleDepositClick = this.handleDepositClick.bind(this)
-  }
-
-  handleDepositClick(e) {
-    e.preventDefault();
-    if (isNaN(this.refs.amount.value)) {
-      console.log("Not a number");
-    }
-    else {
-      let amount = +this.refs.amount.value;
-      let newBalance = this.state.balance + amount;
-      this.setState({
-        balance: newBalance
-      })
-      this.refs.amount.value = '';
-    }
   }
 
   render() {
-    let balanceClass = 'balance';
-    if (this.state.balance === 0) {
+
+    const total = parseInt(this.props.balance)
+
+    let balanceClass = 'balance' ;
+    if (total === 0) {
       balanceClass += ' zero';
     }
 
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className={balanceClass}>${this.state.balance}</div>
+        <div className={balanceClass}>${total}</div>
         <input type="text" placeholder="enter an amount" ref="amount" />
-        <input type="button" value="Deposit" onClick={this.handleDepositClick} />
+        <input
+          type="button"
+          value="Deposit"
+          onClick={(e) => this.props.handleDepositClick(e, this.refs.amount.value, this.props.name)}
+        />
+        <input
+          type="button"
+          value="Withdrawl"
+          onClick={(e) => this.props.handleWithdrawalClick(e, this.refs.amount.value, this.props.name)}
+        />
+        <input
+          type="button"
+          value="Transfer"
+          onClick={() => this.props.handleTransferClick(this.refs.amount.value, this.props.name)}
+        />
       </div>
     )
   }
--- src/App.js
+++ src/App.js
@@ -4,14 +4,99 @@ import logo from './ga.png';
 import Account from './Account';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      checkingBalance: 0,
+      savingsBalance: 0,
+    }
+    this.handleDepositClick = this.handleDepositClick.bind(this)
+    this.handleWithdrawalClick = this.handleWithdrawalClick.bind(this)
+  }
+
+  handleDepositClick(e, amount, name) {
+    e.preventDefault();
+    if (isNaN(amount) || amount < 0) {
+      console.log("Not a number");
+    } else {
+      const inputAmount = parseInt(amount)
+      let newBalance = 0
+      if (name === 'Checking') {
+        newBalance = +this.state.checkingBalance + inputAmount
+        this.setState({
+          checkingBalance: newBalance
+        })
+      } else {
+        newBalance = +this.state.savingsBalance + inputAmount
+        this.setState({
+          savingsBalance: newBalance
+        })
+      }
+    }
+  }
+
+  handleWithdrawalClick(e, amount, name) {
+    e.preventDefault();
+    if (isNaN(amount) || amount < 0) {
+      console.log("Not a number");
+    } else {
+      const inputAmount = parseInt(amount)
+      let newBalance = 0
+      if (name === 'Checking') {
+        newBalance = +this.state.checkingBalance - inputAmount
+        this.setState({
+          checkingBalance: newBalance
+        })
+      } else {
+        newBalance = +this.state.savingsBalance - inputAmount
+        this.setState({
+          savingsBalance: newBalance
+        })
+      }
+    }
+  }
+
+  handleTransferClick = (transAmount, name) => {
+    console.log("handleTransferClick works!")
+    name = name.toLowerCase(name)
+    let current = ''
+    name === 'checking' ? current = this.state.checkingBalance : current = this.state.savingsBalance
+    if (transAmount <= current && transAmount > 0) {
+      if (name === 'checking') {
+        this.setState({
+          checkingBalance: this.state.checkingBalance - parseInt(transAmount),
+          savingsBalance: this.state.savingsBalance + parseInt(transAmount)
+        })
+      } else {
+        this.setState({
+          checkingBalance: this.state.checkingBalance + parseInt(transAmount),
+          savingsBalance: this.state.savingsBalance - parseInt(transAmount)
+        })
+      }
+    }
+  }
+
   render() {
     return (
-      <div id="content">
+      <div>
         <div id="nav">
           <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
           <div id="title">Bank of GA</div>
         </div>
-        <Account name="Checking" />
+        <Account name="Checking"
+          balance={this.state.checkingBalance}
+          handleWithdrawalClick={this.handleWithdrawalClick}
+          handleDepositClick={this.handleDepositClick}
+          transfer={this.state.transferToChecking}
+          handleTransferClick={this.handleTransferClick}
+        />
+        <Account name="Savings"
+          balance={this.state.savingsBalance}
+          handleWithdrawalClick={this.handleWithdrawalClick}
+          handleDepositClick={this.handleDepositClick}
+          transfer={this.state.transferToSavings}
+          handleTransferClick={this.handleTransferClick}
+        />
         <div className="clear"></div>
       </div>
     );
